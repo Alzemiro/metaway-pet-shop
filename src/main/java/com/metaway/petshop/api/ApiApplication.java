e51b08445f0b7034ace3c4e8d696fdd03f50c765
Alzemiro Iago de Moraes Thomaz <alzemiro@supero.com.br>
2023-08-08 17:03:22 -0300

    POM e API

--- src/main/java/com/metaway/petshop/api/ApiApplication.java
+++ src/main/java/com/metaway/petshop/api/ApiApplication.java
@@ -1,9 +1,14 @@
 package com.metaway.petshop.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan
+@AutoConfiguration
 public class ApiApplication {
 
 	public static void main(String[] args) {
